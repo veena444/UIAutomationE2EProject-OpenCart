12f9f5ac0472934da808967d6ad90fc3f3d73cde
Veena Hegde <veena444@gmail.com>
2024-09-26 10:10:23 +0530

    Added cross browser logic,where it reads the browserName from testng.xml

--- UIAutomationE2EProject-OpenCart/src/test/java/com/qa/opencart/base/BaseTest.java
+++ UIAutomationE2EProject-OpenCart/src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriverFactory;
@@ -29,11 +30,16 @@ public class BaseTest {
 	protected ProductInfoPage productInfoPage;
 	protected ShoppingCartPage shoppingCartPage;
 	
+	@Parameters({"browser"}) 
 	@BeforeTest
 	
-	public void setUp() { 
+	public void setUp(String browserName) { 
 		df = new DriverFactory();
 		prop = df.initProp();
+		//check if browser parameter is coming from testng.xml
+		if(browserName != null) { 
+			prop.setProperty("browser", browserName);
+		}
 		driver = df.initDriver(prop);		
 		loginPage = new LoginPage(driver); 
 		softAssert = new SoftAssert();
